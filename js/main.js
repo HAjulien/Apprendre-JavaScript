8f945d9e26ec12d5f2abe9e2e7452811a6f8fc4a
HAjulien <hajulien1@gmail.com>
2022-01-17 16:32:03 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-alert('Hello World');
+// alert('Hello World');
+/* document.open();
+ rajoute texte comme print*/
+document.write('<p class=para> Bonjour le monde ! </p>');
+// document.close();
 
-// rajoute texte
-document.write('Bonjour le monde !');
+
+let start = 'Bonjour ',
+prenom = '', end =' !',
+result;
+
+prenom= prompt('Quel est votre prénom ?');
+result = start + prenom + end;
+alert(result);
+
+console.log(result);
+
+document.open();
+document.write("<div id='monid'>" + result + "</div>");
+ducument.close();
