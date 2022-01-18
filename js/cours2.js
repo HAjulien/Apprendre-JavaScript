7b85dd28b493d16200fa55243a22ea2a6a7235a1
HAjulien <hajulien1@gmail.com>
2022-01-18 14:55:41 +0100

    Update cours2.js

--- js/cours2.js
+++ js/cours2.js
@@ -1,1 +1,14 @@
-let number
+// let number = 0;
+// let output = number++;
+
+// alert(number);
+// document.write(output);
+
+
+let number = 0;
+while (number < 10){
+    number++;       //affiche jusqu'a 10
+    alert(number);
+    // number++;  affiche jusqu'a 9
+}
+
