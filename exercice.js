34691f8e87273114a969b9aa734bdeb5a32b19ca
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-01-21 14:21:24 +0100

    Update exercice.js

--- exercice.js
+++ exercice.js
@@ -7,8 +7,17 @@
 // alert(`Bonjour ${prenom}`);
 
 
-chiffre1= parseInt(prompt('choisissez un chiffre'));
-chiffre2= parseInt(prompt('choisissez un  deuxiemme chiffre'));
+// chiffre1= parseInt(prompt('choisissez un chiffre'));
+// chiffre2= parseInt(prompt('choisissez un  deuxiemme chiffre'));
 
-let result = chiffre1 + chiffre2;
-alert(`le resultat est ${result}`);
+// let result = chiffre1 + chiffre2;
+// alert(`le resultat est ${result}`);
+let age = "";
+age = parseInt(prompt('quel est votre age'));
+
+if (age < 18 ){
+    alert('vous avez fait une erreur')
+    age = parseInt(prompt('quel est votre age'));
+} else{
+    alert('c est bon')
+};
