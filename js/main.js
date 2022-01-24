205e8f2706e63e3020b2511d41bfdb13794762ac
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-01-24 15:48:21 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,18 +5,44 @@ document.write('<p class=para> Bonjour le monde ! </p>');
 // document.close();
 
 
-let start = 'Bonjour ',
-prenom = '', end =' !',
-result;
+// let start = 'Bonjour ',
+// prenom = '', end =' !',
+// result;
 
-prenom= prompt('Quel est votre prénom ?');
-result = start + prenom + end;
-alert(result);
+// prenom= prompt('Quel est votre prénom ?');
+// result = start + prenom + end;
+// alert(result);
 
-console.log(result);
+// console.log(result);
 
-document.open();
-document.write("<div id='monid'>" + result + "</div>");
-ducument.close();
+// document.open();
+// document.write("<div id='monid'>" + result + "</div>");
+// ducument.close();
+
+let myArrey2 = ['sébastien', 'Laurence', 'Ludovic', 'Pauline', 'Guillaume'];
+
+alert(myArrey2.length);
+
+for(let index=0, items=myArrey2.length; index < items; index++){
+    alert( myArrey2[index]);
+}
 
 
+
+// let family = {};
+
+let family = {
+    self: 'Patrick',
+    sister: 'Laurence',
+    brother: 'Ludovic',
+    cousin_1: 'Pauline',
+    cousins_2: 'Guillaume'
+};
+
+alert(family['cousin_1']);
+alert(family['brother']);
+
+// 2 possibilité de recuperer la valeur la propriete plutot le . pour maintement
+
+alert(family.cousins_2);
+alert(family.brother);
