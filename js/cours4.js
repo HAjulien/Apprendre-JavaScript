0cd48806426f5adcf7c48451fedc9b8169ba4c6a
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-01-25 16:02:57 +0100

    main

--- js/cours4.js
+++ js/cours4.js
@@ -18,9 +18,26 @@ alert(link);
 document.getElementById('myColoredDiv').className = 'rose';
 
 let element = document.getElementById('clickme');
+let i = 0;
+
+
+// function change() {
+//     element.innerHTML = i++;
+// }
 
 element.addEventListener('click', function(){
-    alert('vous avez cliqué ! ')
+    // alert('vous avez cliqué ! ')
+    // setInterval(change,1000)
+    let interval = setInterval(function change(){
+        if (i > 10){
+        clearInterval(interval);
+    }
+    element.innerHTML = ++i;
+    },1000) 
 },false);
 
+
+element.addEventListener('mouseover',function(){
+    element.style.backgroundColor = "#00FF00"
+},false);
 
