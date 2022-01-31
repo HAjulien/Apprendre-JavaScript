6f110bb1263a2f55da96b911639c4815171bb3d3
HAjulien <hajulien1@gmail.com>
2022-01-31 10:52:24 +0100

    Update cours4.js

--- js/cours4.js
+++ js/cours4.js
@@ -9,8 +9,9 @@
 let link = document.getElementById('mylink');
 //utilise getter pour recuperer acceseur
 let hyperlink = link.getAttribute('href');
+
 alert(hyperlink);
-//utilise setter pour modifier initiateur
+//utilise setter pour modifier initiateur/mutateur
 link.setAttribute( "href", "http://www.afpa.fr");
 alert(link);
 
@@ -40,4 +41,41 @@ element.addEventListener('click', function(){
 element.addEventListener('mouseover',function(){
     element.style.backgroundColor = "#00FF00"
 },false);
+
+//creer un objet
+class Calcul{
+    constructor (var1, var2){
+        this.produit = var1 * var2;
+    }
+};
+alert (new Calcul(4,5).produit);
+//let calcul1 = new Calcul (4,5).produit;
+//alert(calcul);
+
+
+//on crée objet Person
+class Person{
+    constructor (nick, age, sex, parent, work, friends){
+        this.nick = nick;
+        this.age = age;
+        this.sex = sex;                       //getters
+        this.parent = parent;
+        this.work = work;
+        this.friends = friends;
+    }
+};
+
+
+//on assigne des propiétés a la variable seb
+let seb = new Person ('Sébastien', 25,'m', 'aîné', 'développeur', []);
+alert(seb.nick);
+
+
+seb.nick = 'Bastien';
+seb.age = 23; //change la valeur age
+
+alert(seb.nick + ' a ' + seb.age + ' ans.');
+
+//crée une methode pour ajouter ami
+
 
